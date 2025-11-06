5528860d0039cda023b958d8104569410fc74c0b
Fryyzi <fryyzihnk@gmail.com>
2025-11-06 15:34:23 +0200

    Delete RunScriptsPython

--- Wizzy/Wizzy/Wizzy/obj/Debug/net8.0-windows/Wizzy.AssemblyInfo.cs
+++ Wizzy/Wizzy/Wizzy/obj/Debug/net8.0-windows/Wizzy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wizzy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b0771b9184abf5b8117a8a2861f73917c22ae7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94c0fa95857be0aec8632497e4b9227ea365515")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wizzy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wizzy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
