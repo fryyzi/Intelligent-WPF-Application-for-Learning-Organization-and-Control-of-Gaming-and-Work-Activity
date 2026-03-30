04ab0fd391caf20c30518fe0df1bb5f5e3bfa659
Fryyzi <fryyzihnk@gmail.com>
2026-03-31 00:07:21 +0300

    Correction

--- Wizzy/Wizzy/Wizzy/obj/Debug/net8.0-windows/Wizzy.AssemblyInfo.cs
+++ Wizzy/Wizzy/Wizzy/obj/Debug/net8.0-windows/Wizzy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wizzy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d676c0200ef34c2a434ccec767397994164469c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a6cf808f965253be43847613344c81da286278")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wizzy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wizzy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
